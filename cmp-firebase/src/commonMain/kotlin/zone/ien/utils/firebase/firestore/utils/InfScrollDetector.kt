d5f606c1675541a6ce43eb45af17b9edffb45068
IENGROUND <ericano.rhee@gmail.com>
2026-01-13 04:24:29 +0900

    :sparkles:feat(cmp-firebase): InfScrollDetector 추가

---
+++ cmp-firebase/src/commonMain/kotlin/zone/ien/utils/firebase/firestore/utils/InfScrollDetector.kt
@@ -0,0 +1,61 @@
+package zone.ien.utils.firebase.firestore.utils
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+
+@Composable
+fun InfScrollDetector(
+    listState: LazyListState,
+    isLoading: Boolean,
+    hasMore: Boolean,
+    onLoadNext: () -> Unit
+) {
+    val shouldLoadMore by remember(isLoading, hasMore) {
+        derivedStateOf {
+            val layoutInfo = listState.layoutInfo
+            val lastVisibleItem = layoutInfo.visibleItemsInfo.lastOrNull()
+
+            lastVisibleItem?.index == layoutInfo.totalItemsCount - 1 &&
+                    layoutInfo.totalItemsCount > 0 &&
+                    hasMore &&
+                    !isLoading
+        }
+    }
+
+    LaunchedEffect(shouldLoadMore) {
+        if (shouldLoadMore) {
+            onLoadNext()
+        }
+    }
+}
+
+@Composable
+fun InfScrollDetector(
+    listState: LazyGridState,
+    isLoading: Boolean,
+    hasMore: Boolean,
+    onLoadNext: () -> Unit
+) {
+    val shouldLoadMore by remember(isLoading, hasMore) {
+        derivedStateOf {
+            val layoutInfo = listState.layoutInfo
+            val lastVisibleItem = layoutInfo.visibleItemsInfo.lastOrNull()
+
+            lastVisibleItem?.index == layoutInfo.totalItemsCount - 1 &&
+                    layoutInfo.totalItemsCount > 0 &&
+                    hasMore &&
+                    !isLoading
+        }
+    }
+
+    LaunchedEffect(shouldLoadMore) {
+        if (shouldLoadMore) {
+            onLoadNext()
+        }
+    }
+}
