088cc9d38bbfacc0ccf0346072fb3593d7fd48d9
IENGROUND <ericano.rhee@gmail.com>
2026-01-15 01:20:44 +0900

    :sparkles:feat(cmp-firebase): Lib add

--- cmp-firebase/build.gradle.kts
+++ cmp-firebase/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.kotlin.multiplatform.library)
@@ -19,6 +21,10 @@ kotlin {
             sourceSetTreeName = "test"
         }.configure {
             instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
     iosArm64()
