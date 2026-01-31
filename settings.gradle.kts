5460db0c7504aa4aab066bf26022482ad1d44ce9
IENGROUND <ericano.rhee@gmail.com>
2026-01-31 15:12:13 +0900

    ✨feat(androidApp): 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,7 @@ dependencyResolutionManagement {
 rootProject.name = "ienlab-cmp-library"
 include(":example")
 include(":example:composeApp")
+include(":example:androidApp")
 include(":example:iosApp")
 include(":cmp-ui")
 include(":cmp-date")
