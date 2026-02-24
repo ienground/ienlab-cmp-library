76270f953cd44ab9cd80230c863d08ad8ee1e88b
IENGROUND <ericano.rhee@gmail.com>
2026-02-24 23:20:07 +0900

    ♻️refactor(utils): rename StringCheck.kt to String.kt and add ifEmptyOrNull extension

--- cmp-utils/src/commonMain/kotlin/zone/ien/utils/utils/StringCheck.kt
+++
@@ -1,6 +0,0 @@
-package zone.ien.utils.utils
-
-fun String.checkDecimal() = matches(Regex("^\\d*$"))
-fun String.checkDouble() = matches(Regex("^\\d*(\\.\\d*)?\$"))
-
-fun String.toSafeInt(): Int = toIntOrNull() ?: 0
