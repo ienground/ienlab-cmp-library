2abdfb40139c8853d95c3582fadab239775345e1
IENGROUND <ericano.rhee@gmail.com>
2026-02-24 23:30:42 +0900

    ✨feat(utils): add @Composable overload for ifEmptyOrNull String extension

--- cmp-utils/src/commonMain/kotlin/zone/ien/utils/utils/String.kt
+++ cmp-utils/src/commonMain/kotlin/zone/ien/utils/utils/String.kt
@@ -1,5 +1,6 @@
 package zone.ien.utils.utils
 
+import androidx.compose.runtime.Composable
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
@@ -9,3 +10,6 @@ fun String.checkDouble() = matches(Regex("^\\d*(\\.\\d*)?\$"))
 fun String.toSafeInt(): Int = toIntOrNull() ?: 0
 
 fun String?.ifEmptyOrNull(defaultValue: () -> String) = if (isNullOrEmpty()) defaultValue() else this
+
+@Composable
+fun String?.ifEmptyOrNull(defaultValue: @Composable () -> String) = if (isNullOrEmpty()) defaultValue() else this
