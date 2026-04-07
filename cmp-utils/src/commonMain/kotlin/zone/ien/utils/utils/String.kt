340af08917dc9c979977eb694be86fb11a7f2186
IENGROUND <ericano.rhee@gmail.com>
2026-04-07 15:11:25 +0900

    ♻️ refactor(date): 요일 이름 관련 프로퍼티 및 함수 가시성 변경
    - `weekdayNames` 및 `getWeekdayNames`의 가시성을 `internal`에서 `public`으로 변경합니다.
    - `cmp-utils` 내 `String.kt`에서 불필요한 import 구문을 제거합니다.
    - `libs.versions.toml`에서 `lib-version-name`을 `2.2.0-alpha07`로 업데이트합니다.

--- cmp-utils/src/commonMain/kotlin/zone/ien/utils/utils/String.kt
+++ cmp-utils/src/commonMain/kotlin/zone/ien/utils/utils/String.kt
@@ -1,8 +1,5 @@
 package zone.ien.utils.utils
 
-import androidx.compose.runtime.Composable
-import org.jetbrains.compose.resources.stringResource
-import zone.ien.utils.cmp_utils.generated.resources.Res
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
