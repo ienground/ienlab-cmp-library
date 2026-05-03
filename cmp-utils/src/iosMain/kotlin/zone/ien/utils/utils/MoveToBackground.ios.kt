a6caca45b7922d228161824f26f21618475756a2
IENGROUND <ericano.rhee@gmail.com>
2026-05-03 12:42:57 +0900

    ♻️ refactor(ios): 백그라운드 전환 로직 수정
    iOS에서 앱을 백그라운드로 전환하는 방식을 `performSelector` 호출에서 `UIControl().sendAction` 사용 방식으로 변경합니다.
    
    - `MoveToBackground.ios.kt`: 보다 안정적인 백그라운드 전환을 위해 UIControl 액션 전송 방식으로 구현 변경
    - `example/androidApp/build.gradle.kts`: 불필요한 공백 제거

--- cmp-utils/src/iosMain/kotlin/zone/ien/utils/utils/MoveToBackground.ios.kt
+++ cmp-utils/src/iosMain/kotlin/zone/ien/utils/utils/MoveToBackground.ios.kt
@@ -3,8 +3,13 @@ package zone.ien.utils.utils
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSSelectorFromString
 import platform.UIKit.UIApplication
+import platform.UIKit.UIControl
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun moveToBackground() {
-    UIApplication.sharedApplication.performSelector(aSelector = NSSelectorFromString("suspend"))
+    UIControl().sendAction(
+        NSSelectorFromString("suspend"),
+        to = UIApplication.sharedApplication,
+        forEvent = null
+    )
 }
