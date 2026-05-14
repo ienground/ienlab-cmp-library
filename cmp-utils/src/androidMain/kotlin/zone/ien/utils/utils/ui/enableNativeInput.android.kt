dbf7d8a4fa44f8b874cefcf48a1e2ae33d3eb50e
IENGROUND <my@ien.zone>
2026-05-15 01:25:41 +0900

    ♻️ refactor(ui): lifecycle 버전 업데이트 및 코드 정리
    - libs.versions.toml의 lifecycle 버전을 2.11.0-beta01로 업데이트합니다.
    - iosMain의 enableNativeInput 함수에서 불필요한 KDoc 주석을 제거합니다.
    - Android 및 iOS 소스 코드의 불필요한 공백 및 포맷팅을 정리합니다.

--- cmp-utils/src/androidMain/kotlin/zone/ien/utils/utils/ui/enableNativeInput.android.kt
+++ cmp-utils/src/androidMain/kotlin/zone/ien/utils/utils/ui/enableNativeInput.android.kt
@@ -1,5 +1,6 @@
 package zone.ien.utils.utils.ui
 
 import androidx.compose.foundation.text.KeyboardOptions
+
 // Android does not require additional configuration for native input.
 actual fun KeyboardOptions.enableNativeInput(): KeyboardOptions = this
